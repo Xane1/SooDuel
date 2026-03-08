01f1bea2177484cb0b0c488b266c6ff5c2f661eb
Xane1 <xedr1g23@soton.ac.uk>
2026-03-08 21:33:38 +0000

    UI improvements
    - Added controller support for main menu
    - Added backwards for menu
    - Got rid of old unused mp3s
    - Reorganised audio assets & imported new mp3s

--- Assets/Scripts/GameControllerScripts/TitleScreenScript.cs
+++ Assets/Scripts/GameControllerScripts/TitleScreenScript.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 public class TitleScreenScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     public void SinglePlayerButton()
     {
         Time.timeScale = 1f;
@@ -36,5 +38,18 @@ public class TitleScreenScript : MonoBehaviour
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene("VersusScene");
+    }
+    void Update()
+    {
+        if (Keyboard.current.escapeKey.wasPressedThisFrame ||
+            Gamepad.current.buttonEast.wasPressedThisFrame)
+        {
+            GoBack();
+        }
+    }
+
+    void GoBack()
+    {
+        SceneManager.LoadScene("TitleScreen");
     }
 }
